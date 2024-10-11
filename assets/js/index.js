97549ab72ea4f39daf638b6ed81fd99dbe382d81
ramees <ramees@gmail.com>
2024-10-11 20:52:04 +0530

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+
+
+document.querySelector('button').addEventListener("click",()=>{
+    document.querySelector('dialog').showModal()
+})
+document.getElementsByClassName('conatin')[0].addEventListener("click",()=>{
+    document.getElementsByClassName('conatin')[0].classList.toggle("mystyle");
+})
+
